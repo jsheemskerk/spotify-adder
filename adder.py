b1dd568e6ca07b7c016c6be3ebeb8dc15629e33e
jsheemskerk <joranheemskerk@gmail.com>
2022-04-19 22:05:43 +0200

    Add connection error handling

--- adder.py
+++ adder.py
@@ -2,19 +2,20 @@
 import sys
 import spotipy
 import spotipy.util as util
-import pprint
 import time
 import logging
 import os
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
+import requests
 
-
-pp=pprint.PrettyPrinter(indent=4)
 logging.basicConfig(format='%(asctime)s %(message)s', 
         datefmt='%d/%m/%Y %H:%M:%S', level=logging.INFO)
 
 load_dotenv() # optional .env file
+
+failurecount = 0
+expb = 1
 
 playlist_id = os.getenv("PLAYLIST_ID")
 scope = ['user-read-currently-playing', 
@@ -33,18 +34,50 @@ while(pl["next"]):
 
 logging.info("Found %s tracks", len(tracks))
 
+
 while True:
-    cur = sp.currently_playing()
-    if cur is None:
-        logging.info("Currently not playing anything")
-        time.sleep(110)
-    else:
-        trackid = cur["item"]["id"]
-        if trackid not in tracks:
-            tracks.append(trackid)
-            logging.info("Adding track: %s - %s",
-                    cur["item"]["artists"][0]["name"], cur["item"]["name"])
-            sp.playlist_add_items(playlist_id, [trackid], position=None)
-        # else:
-        #     logging.info("Item is in playlist already")
-    time.sleep(10)
+    try:
+        cur = sp.currently_playing()
+        expb = 1
+        if cur is None:
+            # logging.info("Currently not playing anything")
+            time.sleep(90)
+        else:
+            if cur["item"] is not None:
+                trackid = cur["item"]["id"]
+                if trackid not in tracks:
+                    sp.playlist_add_items(playlist_id, [trackid], position=None)
+                    tracks.append(trackid)
+                    logging.info("Adding track: %s - %s",
+                            cur["item"]["artists"][0]["name"], cur["item"]["name"])
+            # else:
+            #     logging.info("Item is in playlist already")
+    except (requests.ConnectionError, requests.ReadTimeout):
+        failurecount += 1
+        logging.error("Connection error, expb: %d, total failures: %d", expb, failurecount)
+        if expb == 1:
+            expb = 2
+            continue
+        elif expb == 2:
+            expb = 4
+            time.sleep(60)
+            continue
+        elif expb == 4:
+            expb = 8
+            time.sleep(600)
+            continue
+        else:
+            logging.error("Failed too often")
+            exit(1)
+    # except TypeError:
+    #     logging.error("cur: %s", cur)
+    #     time.sleep(5)
+    #     continue
+    # except Exception as e:
+    #     logging.error("%s", e)
+    #     logging.error("General error?")
+    #     failurecount += 1
+    #     logging.error("Failures: %d", failurecount)
+    #     time.sleep(300)
+    #     continue
+    time.sleep(30)
